01be89b1f52d77192355459bec4581215f4bfd98
Troia <vadym.queensoft@gmail.com>
2023-06-01 13:36:32 +0300

    fd

--- app/code/MD/CloseInPrice/Block/Widget/SimilarProducts.php
+++ app/code/MD/CloseInPrice/Block/Widget/SimilarProducts.php
@@ -6,6 +6,9 @@ use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Magento\Framework\View\Element\Template;
 use Magento\Widget\Block\BlockInterface;
 use Magento\Catalog\Block\Product\View;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+
 class SimilarProducts extends Template implements BlockInterface
 {
     protected $_template = 'widget/similar_products.phtml';
@@ -13,18 +16,24 @@ class SimilarProducts extends Template implements BlockInterface
     protected $collectionFactory;
     protected $visibility;
     protected $product;
+    protected $order;
+    protected $searchCriteriaBuilder;
 
     public function __construct(
         Template\Context $context,
         CollectionFactory $collectionFactory,
         Visibility $visibility,
         View $product,
+        OrderRepositoryInterface $order,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
         array $data = []
     ) {
         parent::__construct($context, $data);
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
         $this->product = $product->getProduct();
         $this->collectionFactory = $collectionFactory;
         $this->visibility = $visibility;
+        $this->order = $order;
     }
 
     public function getPriceDifference()
@@ -32,26 +41,38 @@ class SimilarProducts extends Template implements BlockInterface
         return (float) $this->getData('price_difference');
     }
 
+    public function getProduct(){
+        return $this->product;
+    }
+
     public function getSimilarProducts()
     {
-        $categoryId = $this->product->getCategoryIds()[0] ?? false;
-        if (!$categoryId) {
-            return [];
+        $categoryId = $this->product->getCategoryIds();
+        $currentDate = date('Y-m-d');
+
+        $orderList = $this->order->getList(
+            $this->searchCriteriaBuilder
+                ->addFilter('created_at', $currentDate, 'gteq')
+                ->create()
+        )->getItems();
+        
+        $filteredOrderList = [];
+        foreach ($orderList as $order) {
+            $orderItems = $order->getItems();
+        
+            foreach ($orderItems as $orderItem) {
+                $productCategories = $orderItem->getProduct()->getCategoryIds();
+        
+                foreach ($productCategories as $productCategoryId) {
+                    if (in_array($productCategoryId, $categoryId)) {
+                        $filteredOrderList[] = $orderItem->getProduct();
+                        break;
+                    }
+                }
+            }
         }
+        
 
-        $productCollection = $this->collectionFactory->create();
-        $productCollection->addCategoriesFilter(['eq' => $categoryId]);
-        $productCollection->setVisibility($this->visibility->getVisibleInCatalogIds());
-        $productCollection->addFinalPrice();
-        $productCollection->addAttributeToSelect('*');
-
-        $price = $this->product->getFinalPrice();
-        $priceDifference = $this->getPriceDifference();
-        $minPrice = $price - $priceDifference;
-        $maxPrice = $price + $priceDifference;
-
-        $productCollection->addAttributeToFilter('price', ['between' => [$minPrice, $maxPrice]]);
-
-        return $productCollection;
+        return $filteredOrderList;
     }
 }
